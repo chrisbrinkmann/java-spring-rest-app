142b909e988074bb019dba07cd2d622b7082ac2d
chrisbrinkmann <brinkmcd@gmail.com>
2019-11-30 11:57:18 -0500

    autowire inject UserService field in UserController

---
+++ src/main/java/com/brinkmcd/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.brinkmcd.app.ws.service.impl;
+
+import com.brinkmcd.app.ws.service.UserService;
+import com.brinkmcd.app.ws.shared.dto.UserDto;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
